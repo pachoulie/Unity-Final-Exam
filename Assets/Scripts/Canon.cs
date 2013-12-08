f8d313258515ef2f526bdc5129ffc5e711856577
regnarock <thomas.wilgenbus@gmail.com>
2013-12-08 23:26:11 +0900

    Now canon is perfectly working.

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Canon : MonoBehaviour {
 	
+	public float power;
 	public float minAngleLeftX;
 	public float maxAngleRightX;
 	public GameObject ballEmiter;
 	public GameObject ball;
 	public float angleX;
 	
-	GameObject ballToLaunch;
 	GameObject nextBall;
 	
 	// Use this for initialization
@@ -18,13 +18,10 @@ public class Canon : MonoBehaviour {
        	rot.eulerAngles = new Vector3(0, 0, 0);
        	transform.rotation=rot;
 		transform.RotateAround(transform.position, new Vector3(1, 0, 0), angleX);
-		ballToLaunch = Instantiate(ball, ballEmiter.transform.position, Quaternion.identity);
-		nextBall.renderer.isVisible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ballToLaunch.SetActive = false;
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			if ( angleX < maxAngleRightX ) {
 				transform.RotateAround(transform.position, new Vector3(1, 0, 0), 1f);
@@ -38,10 +35,10 @@ public class Canon : MonoBehaviour {
 			}
 		}
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			nextBall = Instantiate(ball, ballEmiter.transform.position, Quaternion.identity);
-			nextBall.renderer.isVisible = false;
-			ballToLaunch.SetActive = true;
-			ballToLaunch = nextBall;
+			nextBall = (GameObject) Instantiate(ball, ballEmiter.transform.position, Quaternion.identity);
+			Vector3 dir = ballEmiter.transform.position - transform.position;
+			dir *= power;
+			nextBall.rigidbody.AddForce(dir, ForceMode.Impulse);
 		}
 	}
 }
