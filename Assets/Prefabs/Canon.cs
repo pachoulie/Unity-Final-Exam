4e5163b9917b844e52c416d898b27eb36018fbe8
regnarock <thomas.wilgenbus@gmail.com>
2013-12-09 16:26:39 +0900

    Basic work

---
+++ Assets/Prefabs/Canon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canon : MonoBehaviour {
+	
+	public float minAngleLeftX;
+	public float maxAngleRightX;
+	public GameObject ballEmiter;
+	public float angleX;
+	
+	// Use this for initialization
+	void Start () {
+		Quaternion rot=new Quaternion();
+       	rot.eulerAngles = new Vector3(0, 0, 0);
+       	transform.rotation=rot;
+		transform.RotateAround(transform.position, new Vector3(1, 0, 0), angleX);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if ( angleX < maxAngleRightX ) {
+				transform.RotateAround(transform.position, new Vector3(1, 0, 0), 1f);
+				angleX++;
+			}
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			if ( angleX > minAngleLeftX ) {
+				transform.RotateAround(transform.position, new Vector3(1, 0, 0), -1f);
+				angleX--;
+			}
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			
+		}
+	}
+}
