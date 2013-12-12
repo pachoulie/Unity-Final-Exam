796e3a0b29de3f21c177b3160c67e984bbf79c43
pachoulie <souy.angelina@gmail.com>
2013-12-12 19:22:57 +0900

    Add Cleared Level Animation
    + Enter Event to Pass the Leve

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -4,18 +4,25 @@ using System.Collections;
 public class Star : MonoBehaviour {
 	
 	public string SceneName;
+	public bool gameFinished;
 	
 	// Use this for initialization
 	void Start () {
+		gameFinished = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (gameFinished == true){
+			if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey ("enter"))
+				Application.LoadLevel (SceneName);
+		}
 	}
 	
 	void OnTriggerEnter() {
-		Application.LoadLevel (SceneName);
+		GameObject	clearedLevelPlane = GameObject.Find("Plane");
+		clearedLevelPlane.animation.Play();
+		gameFinished = true;
 	}
 }
