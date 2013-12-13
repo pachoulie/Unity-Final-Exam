d1e0d9f131a78a790ace0c3b6544d159275ff1c2
regnarock <thomas.wilgenbus@gmail.com>
2013-12-13 16:59:50 +0900

    Awesome laser ! Psiouuuu

---
+++ Assets/Scripts/Crystal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crystal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log(collision.gameObject.name);
+	}
+	
+	void OnCollisionExit(Collision collision) {
+		
+	}
+}
