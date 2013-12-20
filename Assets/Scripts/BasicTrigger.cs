db416869abb24cdb8e53a536b972754afc107412
regnarock <thomas.wilgenbus@gmail.com>
2013-12-20 18:30:09 +0900

    Finished the worse level ever ><'

--- Assets/Scripts/BasicTrigger.cs
+++ Assets/Scripts/BasicTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Door : MonoBehaviour, ITriggered {
+public class BasicTrigger : MonoBehaviour, ITriggered {
 	
 	public string AnimationName;
 	
